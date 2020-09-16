3c05d88689719a96e462660310bda7d871d4fab4
Morten Laukvik <morten.laukvik@nav.no>
2020-09-16 15:44:29 +0200

    SonarQube. Bruk test.inclusions

--- run-sonar.js
+++ run-sonar.js
@@ -11,6 +11,7 @@ scanner(
             'sonar.exlusions': 'src/index.ts,src/app.tsx',
             'sonar.javascript.lcov.reportPaths': './coverage/lcov.info',
             'sonar.test.inclusions': 'src/**/*.test.ts,src/**/*.test.tsx',
+            'sonar.coverage.exclusions': 'src/**/*.test.tsx'
         }
     },
     () => process.exit()
