d852d43632f838aaf686042ebdce0de5c2126820
Morten Laukvik <morten.laukvik@nav.no>
2020-09-16 10:20:30 +0200

    SonarQube. Eksluder tester i coverage

--- run-sonar.js
+++ run-sonar.js
@@ -8,7 +8,8 @@ scanner(
             'sonar.organization': 'navit',
             'sonar.projectKey': 'navikt_helse-arbeidsgiver-felles-frontend',
             'sonar.sources': 'src',
-            'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info'
+            'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+            'sonar.javascript.exclusions': '*.test.*'
         }
     },
     () => process.exit()
