18c49a52381a31715c27d278f5cbc48a29832963
Morten Laukvik <morten.laukvik@nav.no>
2020-09-16 14:01:57 +0200

    Dummy test

--- src/Side.tsx
+++ src/Side.tsx
@@ -9,6 +9,18 @@ export const tull = () => {
     return 'Tullete';
 }
 
+export const tull2 = () => {
+    return 'Tullete';
+}
+
+export const tull3 = () => {
+    return 'Tullete';
+}
+
+export const ganging = (a: number, b: number) => {
+    return a * b;
+}
+
 export const Side = (props: SideProps) => {
     return (<div className='side'>{ props.children ? props.children : 'Tull' }</div>);
 };
