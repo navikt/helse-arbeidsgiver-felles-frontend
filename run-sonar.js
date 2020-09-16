e0c6f66be0b900aaa5c912ba94450b6adfe657a3
Morten Laukvik <morten.laukvik@nav.no>
2020-09-16 15:33:10 +0200

    SonarQube. Bruk test.inclusions

--- run-sonar.js
+++ run-sonar.js
@@ -9,7 +9,6 @@ scanner(
             'sonar.projectKey': 'navikt_helse-arbeidsgiver-felles-frontend',
             'sonar.sources': 'src',
             'sonar.exlusions': 'src/*.ts,src/app.tsx',
-            'sonar.javascript.file.suffixes': '.ts,.tsx',
             'sonar.javascript.lcov.reportPaths': './coverage/lcov.info',
             'sonar.test.inclusions': '**/src/**/*.test.ts,**/src/**/*.test.tsx',
             'sonar.test.exclusions': 'src/*.ts,src/app.tsx',
