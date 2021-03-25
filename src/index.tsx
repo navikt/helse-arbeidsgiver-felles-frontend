3ef56182a80fe1accfacca747b6de3c3e115bb1d
Morten Laukvik <morten.laukvik@nav.no>
2021-03-25 12:38:37 +0100

    Ikke bruk less

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,22 @@ import { fetchFeatureToggles } from './featureToggle/fetchFeatureToggles';
 import FeatureToggleProvider from './featureToggle/FeatureToggleContext';
 import { EnvironmentProvider, useEnvironment } from './context/EnvironmentContext';
 import { LoginProvider, useLogin } from './context/LoginContext';
-import './style/index.less';
+import 'nav-frontend-core/dist/main.css';
+import 'nav-frontend-typografi-style/dist/main.css';
+import 'nav-frontend-skjema-style/dist/main.css';
+import 'nav-frontend-knapper-style/dist/main.css';
+import 'nav-frontend-hjelpetekst-style/dist/main.css';
+import 'nav-frontend-alertstriper-style/dist/main.css';
+import 'nav-frontend-grid-style/dist/main.css';
+import 'nav-frontend-chevron-style/dist/main.css';
+import 'nav-frontend-lenker-style/dist/main.css';
+import 'nav-frontend-paneler-style/dist/main.css';
+import 'nav-frontend-spinner-style/dist/main.css';
+import 'nav-frontend-veileder-style/dist/main.css';
+import 'nav-frontend-veilederpanel-style/dist/main.css';
+import 'nav-frontend-modal-style/dist/main.css';
+import 'nav-frontend-popover-style/dist/main.css';
+import 'nav-frontend-tabell-style/dist/main.css';
 
 /**
  * Alle klasser som skal eksponeres utad via pakkesystemet til NPM skal legges
