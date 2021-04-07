a07418e6b1f35521654906d442e1ec99adcb2b43
Morten Laukvik <morten.laukvik@nav.no>
2021-04-07 13:42:11 +0200

    Fjern ubrukt dependencies

---
+++ src/components/HjelpeLabel/HjelpeLabel.tsx
@@ -0,0 +1,16 @@
+import Hjelpetekst from 'nav-frontend-hjelpetekst';
+import React from 'react';
+
+interface HjelpeLabelProps {
+  label: string;
+  children: any;
+}
+
+const HjelpeLabel = (props: HjelpeLabelProps) => (
+  <div style={{ display: 'flex' }}>
+    {props.label}
+    <Hjelpetekst style={{ marginLeft: '0.5rem' }}>{props.children}</Hjelpetekst>
+  </div>
+);
+
+export default HjelpeLabel;
