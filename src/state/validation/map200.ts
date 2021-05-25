577af0df3cfa199b257a2c011dee6c8c9192be9b
Morten Laukvik <morten.laukvik@nav.no>
2021-05-25 15:40:11 +0200

    state

---
+++ src/state/validation/map200.ts
@@ -0,0 +1,15 @@
+import { ValidationState } from './ValidationState';
+import ValidationResponse from './ValidationResponse';
+
+/**
+ * Some ok - some not
+ */
+const map200 = (state: ValidationState, response: ValidationResponse): ValidationState => {
+  state.kvittering = state.feilmeldinger.length === 0;
+  state.progress = false;
+  state.serverError = false;
+  state.error = false;
+  return state;
+};
+
+export default map200;
