a8a0302d902a076c60d0f9ed02b822d3e46b6e5e
Morten Laukvik <morten.laukvik@nav.no>
2021-05-26 15:59:57 +0200

    Feilmeldingspanel

--- src/components/Feilmeldingspanel/Feilmeldingspanel.tsx
+++ src/components/Feilmeldingspanel/Feilmeldingspanel.tsx
@@ -2,17 +2,33 @@ import Panel from 'nav-frontend-paneler';
 import { Feiloppsummering, FeiloppsummeringFeil } from 'nav-frontend-skjema';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Locale } from '../../locale/Locale';
 
 interface FeilmeldingspanelProps {
   feilmeldinger: Array<FeiloppsummeringFeil>;
 }
+
+enum FeilmeldingspanelKeys {
+  FEILMELDINGSPANEL_INFO = 'FEILMELDINGSPANEL_INFO'
+}
+
+export const FeilmeldingspanelLanguage: Record<FeilmeldingspanelKeys, Locale> =
+  {
+    FEILMELDINGSPANEL_INFO: {
+      nb: 'For å gå videre må du rette opp følgende:',
+      en: 'To proceed, you must correct the following:'
+    }
+  };
 
 const Feilmeldingspanel = (props: FeilmeldingspanelProps) => {
   const { t } = useTranslation();
   if (props.feilmeldinger && props.feilmeldinger.length > 0) {
     return (
       <Panel>
-        <Feiloppsummering tittel={t('FEILMELDINGSPANEL')} feil={props.feilmeldinger} />
+        <Feiloppsummering
+          tittel={t(FeilmeldingspanelKeys.FEILMELDINGSPANEL_INFO)}
+          feil={props.feilmeldinger}
+        />
       </Panel>
     );
   } else {
