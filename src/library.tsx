bc16d291627ffb9f42cbbea360a619379da425ed
Morten Laukvik <morten.laukvik@nav.no>
2021-05-28 11:07:16 +0200

    Bruk library for å eksponere klasser

---
+++ src/library.tsx
@@ -0,0 +1,31 @@
+import InnloggetSide from './components/Side/InnloggetSide';
+import Side from './components/Side/Side';
+import DatoVelger from './components/DatoVelger/DatoVelger';
+import LoginExpiryAPI from './api/loginexpiry/LoginExpiryAPI';
+import {
+  ArbeidsgiverProvider,
+  useArbeidsgiver
+} from './context/arbeidsgiver/ArbeidsgiverContext';
+import ArbeidsgiverAPI from './api/arbeidsgiver/ArbeidsgiverAPI';
+import ArbeidsgiverStatus from './context/arbeidsgiver/ArbeidsgiverStatus';
+
+/**
+ * Alle klasser som skal eksponeres utad via pakkesystemet til NPM skal legges
+ * inn som en komma separert liste som export neders i denne file.
+ *
+ * app.tsx blir ikke eksponert utad og skal kun demonstrere bruksmønster internt.
+ *
+ */
+
+
+export {
+  InnloggetSide,
+  Side,
+  DatoVelger,
+  ArbeidsgiverProvider, useArbeidsgiver,
+  ArbeidsgiverAPI,
+  ArbeidsgiverStatus,
+  LoginExpiryAPI,
+}
+
+
