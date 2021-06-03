9dd02832b64aa0a71216349f77ca8462c86cdbba
Morten Laukvik <morten.laukvik@nav.no>
2021-06-03 15:17:05 +0200

    setupTests.ts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import { toHaveNoViolations } from 'jest-axe';
 import 'whatwg-fetch'
+import {jest} from '@jest/globals';
 
 // Extend the functionality to support axe
 expect.extend(toHaveNoViolations);
